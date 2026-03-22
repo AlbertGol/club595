779788e273b9386b704de8740443c2fa6671ac39
AlbertGol <vaggolds@gmail.com>
2026-03-22 12:29:55 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDFvlQ-tr9hARes2Z8M5iK8h2bJQlWNJD8",
+    authDomain: "club595.firebaseapp.com",
+    projectId: "club595",
+    storageBucket: "club595.firebasestorage.app",
+    messagingSenderId: "452572247063",
+    appId: "1:452572247063:web:040230b948ada5b9a69852"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// Тут будет логика записи (Neck massage, Detox и т.д.)
+// Я напишу подробный код в следующем сообщении, чтобы ты не запутался
+console.log("Мозг клиента запущен");
